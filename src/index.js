41efa92f46a061ba069854c257aa2e7100ce9496
Basi Yonah Bwambale <basiyonah@gmail.com>
2023-06-28 12:05:37 -0400

    Remove unnecessary files and create our first component Hello React World!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+//1) Import the react and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-// Test GIT Intergration
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//2) Get a reference to the the div with ID root
+const el = document.getElementById('root')
+//3) Tell React to take Control of that element
+const root = ReactDOM.createRoot(el);
+//4) Create a Component
+function App(){
+    return <h1>Hello React World!</h1>
+}
+//5) Show the Component on the screen
+root.render(<App />)
