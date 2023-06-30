99d27f00cb471f03cae4d3ae25e4f8ce7e0fb6f8
Basi Yonah Bwambale <basiyonah@gmail.com>
2023-06-30 16:57:41 -0400

    Simple App Demoing UserState and array destructuring

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
-import 'bulma/css/bulma.css'
-import ProfileCard from './ProfileCard';
-import AlexaImage from './images/alexa.png'
-import CortanaImage from './images/cortana.png'
-import SiriImage from './images/siri.png'
-function App() {
+import { useState } from 'react'
+
+function App(){
+    const [count, setCount] = useState(0);
+    
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
     return (
         <div>
-
-<section className="hero is-primary">
-    <div className="hero-body">
-        <p className="title">Personal Digital Assistants</p>
-    </div>
-</section>
-            <div className="container">
-                <section className="section">
-                    <div className="columns">
-
-                        <div className="colums is-4">
-                            <ProfileCard 
-                            title="Alexa" 
-                            handle="@Alexa99" 
-                            imageSrc={AlexaImage} 
-                            decription="Alexa Was Created by Amazon and helps you buy things"/>
-                        </div>
-
-                        <div className="colums is-4">
-                            <ProfileCard 
-                            title="Cortana" 
-                            handle="@Cortana32" 
-                            imageSrc={CortanaImage} 
-                            decription="Cortana Was Created by Microsoft and helps search things on windows"
-                            />
-                        </div>
-
-                        <div className="colums is-4">
-                            <ProfileCard title="Siri" 
-                            handle="@Siri01" 
-                            imageSrc={SiriImage} 
-                            decription="Alexa Was Created by Aple and helps you play music from any source"
-                            />
-                        </div>
-
-                    </div>
-                </section>
-            </div>
+          <button onClick={handleClick}>Add Animal</button>
+          <div>
+            Number of Animals: {count}
+          </div>
         </div>
     );
 }
 
-export default App;
+export default App
